1d15a9e69cd9e3404dc53a0979921f3672e94013
Claire Moore <kurennon@gmail.com>
2023-07-03 11:52:03 -0700

    feat: Run the cycles faster
    Configurable, with default at 700 opcodes per second

--- src/bin/chip8_run.rs
+++ src/bin/chip8_run.rs
@@ -1,4 +1,9 @@
-use std::{fs::File, io::Read, path::PathBuf, time::Duration};
+use std::{
+    fs::File,
+    io::Read,
+    path::PathBuf,
+    time::{Duration, Instant},
+};
 
 use clap::Parser;
 use log::error;
@@ -12,18 +17,26 @@ struct Chip8RunArgs {
     // Use the original Chip-8 shift with Vx = Vy
     #[arg(short, long)]
     legacy_shift: bool,
+    #[arg(short, long, default_value_t = 700)]
+    opcodes_per_second: u32,
 }
 
 fn main() {
     env_logger::init();
 
     let args = Chip8RunArgs::parse();
+
     let mut rom = File::open(args.program_path).unwrap();
     let mut program = vec![];
     rom.read_to_end(&mut program).unwrap();
+
     let mut executor = Executor::new(args.legacy_shift);
     executor.load_program(&program).unwrap();
+
+    let cycle_time = Duration::from_secs(1) / args.opcodes_per_second;
+
     loop {
+        let start = Instant::now();
         if let Err(error) = executor.execute_once() {
             error!("{error}");
         }
@@ -32,6 +45,7 @@ fn main() {
             println!("{}", display);
             display.render();
         }
-        std::thread::sleep(Duration::from_millis(100));
+        let run_elapsed = start.elapsed();
+        std::thread::sleep(cycle_time - run_elapsed);
     }
 }
