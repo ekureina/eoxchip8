5e9d9bb43cd4294f1a5e82000f6f5e9088bcc08a
Claire Moore <kurennon@gmail.com>
2025-09-05 10:01:30 -0700

    Migrate to Iced 0.12
    Old code that was untouched for a bit, but adding back in.

--- src/bin/chip8_run.rs
+++ src/bin/chip8_run.rs
@@ -1,13 +1,25 @@
-use std::{
-    fs::File,
-    io::Read,
-    path::PathBuf,
-    time::{Duration, Instant},
-};
+use std::cell::RefCell;
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+use std::time::{Duration, Instant};
 
 use clap::Parser;
 use eoxchip8::core::cpu::main::Executor;
-use log::error;
+use iced::widget::Text;
+use iced::{executor, time, Font, Length};
+use iced::{Application, Command, Element, Settings, Theme};
+use log::{error, info};
+
+pub fn main() -> iced::Result {
+    env_logger::init();
+
+    let args = Chip8RunArgs::parse();
+    let mut settings = Settings::with_flags(args);
+    settings.default_font = Font::MONOSPACE;
+
+    EoxChip8GUI::run(settings)
+}
 
 #[derive(Debug, Parser, PartialEq, Eq, PartialOrd, Ord)]
 #[command(author, version, about)]
@@ -21,31 +33,77 @@ struct Chip8RunArgs {
     opcodes_per_second: u32,
 }
 
-fn main() {
-    env_logger::init();
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct EoxChip8GUI {
+    executor: RefCell<Executor>,
+    cycle_time: Duration,
+}
 
-    let args = Chip8RunArgs::parse();
+#[derive(Debug, Clone)]
+enum EoxMessage {
+    Tick(Instant),
+}
 
-    let mut rom = File::open(args.program_path).unwrap();
-    let mut program = vec![];
-    rom.read_to_end(&mut program).unwrap();
+impl Application for EoxChip8GUI {
+    type Executor = executor::Default;
+    type Flags = Chip8RunArgs;
+    type Message = EoxMessage;
+    type Theme = Theme;
 
-    let mut executor = Executor::new(args.legacy_shift);
-    executor.load_program(&program).unwrap();
+    fn new(flags: Self::Flags) -> (Self, Command<Self::Message>) {
+        let mut rom = File::open(flags.program_path).unwrap();
+        let mut program = vec![];
+        rom.read_to_end(&mut program).unwrap();
 
-    let cycle_time = Duration::from_secs(1) / args.opcodes_per_second;
+        let mut executor = Executor::new(flags.legacy_shift);
+        executor.load_program(&program).unwrap();
 
-    loop {
-        let start = Instant::now();
-        if let Err(error) = executor.execute_once() {
-            error!("{error}");
+        let cycle_time = Duration::from_secs(1) / flags.opcodes_per_second;
+
+        (
+            EoxChip8GUI {
+                executor: RefCell::new(executor),
+                cycle_time,
+            },
+            Command::none(),
+        )
+    }
+
+    fn title(&self) -> String {
+        String::from("ExoChip8")
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            EoxMessage::Tick(instant) => {
+                info!("{instant:?}");
+
+                if let Err(error) = self.executor.borrow_mut().execute_once() {
+                    error!("{error}");
+                }
+            }
         }
+        Command::none()
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        let mut executor = self.executor.borrow_mut();
         let display = executor.get_display_mut();
-        if display.has_changed() {
-            println!("{}", display);
-            display.render();
-        }
-        let run_elapsed = start.elapsed();
-        std::thread::sleep(cycle_time - run_elapsed);
+        let display_text = format!("{}", display);
+        display.render();
+        Text::new(display_text)
+            .width(Length::Fill)
+            .horizontal_alignment(iced::alignment::Horizontal::Center)
+            .vertical_alignment(iced::alignment::Vertical::Center)
+            .height(Length::Fill)
+            .into()
+    }
+
+    fn subscription(&self) -> iced::Subscription<Self::Message> {
+        time::every(self.cycle_time).map(EoxMessage::Tick)
+    }
+
+    fn theme(&self) -> Self::Theme {
+        Theme::Dark
     }
 }
